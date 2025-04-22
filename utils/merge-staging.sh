c25b5bd104e9575194c482fbfd71a046f93817be
Jayson Espley <jayson@yorkshire3d.co.uk>
2025-04-22 02:50:23 +0100

    Add script to automate merging staging branch into main branch

---
+++ utils/merge-staging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make sure we have the latest changes
+git fetch --all
+
+# Checkout main branch
+git checkout main
+
+# Make sure it's up to date
+git pull origin main
+
+# Merge staging into main
+git merge staging
+
+# Push the changes to remote
+git push origin main
+
+# Optionally switch back to staging
+git checkout staging
+
+echo "Successfully merged staging into main"
